141692a8a4d0aa0ab3753042d44350563c7ec45f
debuelvas <120409484+debuelvas@users.noreply.github.com>
2023-01-17 14:13:08 -0500

    Delete l10n_co_payroll directory

--- l10n_co_payroll/models/hr_leave_allocation.py
+++
@@ -1,6 +0,0 @@
-from odoo import api,fields,models
-
-class HolidaysAllocation(models.Model):
-    """ Allocation Requests Access specifications: similar to leave requests """
-    _inherit = "hr.leave.allocation"
-    saldo = fields.Boolean(string="Es saldo",default=False)
